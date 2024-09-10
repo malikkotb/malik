f5908945011c598255d537c50bac123cd7603739
malikkotb <malikkotb@icloud.com>
2024-09-10 09:57:16 +0200

    setup

--- app/page.js
+++ app/page.js
@@ -3,8 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <section className="justify-center flex items-center landingSection bg-[#e5e4e0] h-screen">
+      {/* TODO: add grainy noise background as ::before on body */}
+      <section className="justify-center flex items-center landingSection bg-white h-screen">
         <div className="flex flex-col tracking-tight font-semibold leading-tight text-[5vw] text-center text-black uppercase">
+          <span>Malik Kotb</span>
+          <span>Independent Web</span>
+          <span>Developer</span>
+          <span>Based in Paris</span>
+        </div>
+      </section>
+      <section className="justify-center flex items-center landingSection bg-[#1d1c1c] h-screen">
+        <div className="flex flex-col tracking-tight font-semibold leading-tight text-[5vw] text-center text-white uppercase">
           <span>Malik Kotb</span>
           <span>Independent Web</span>
           <span>Developer</span>
