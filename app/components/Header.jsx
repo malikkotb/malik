6cc836fe183472480a7f64487622614082389512
malikkotb <malikkotb@icloud.com>
2024-09-13 11:08:38 +0200

    header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import FlipLink from "./FlipLink";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="borderr flex text-xs justify-between w-full fixed p-4">
+      <h1>MALIK KOTB</h1>
+      <nav className="flex gap-2">
+        <FlipLink href="/blog">BLOG</FlipLink>
+        <FlipLink href="/blog">SERVICES</FlipLink>
+        <FlipLink href="/blog">WORKS</FlipLink>
+        <FlipLink href="/blog">CONTACT</FlipLink>
+      </nav>
+    </div>
+  );
 }
