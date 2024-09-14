4df1750138cb9e44193704382d6bbd68b1f294fc
malikkotb <malikkotb@icloud.com>
2024-09-14 19:25:36 +0200

    card

--- components/ProjectsSection/Card/Card.jsx
+++ components/ProjectsSection/Card/Card.jsx
@@ -1,47 +1,66 @@
 "use client";
 import Image from "next/image";
 import styles from "./style.module.scss";
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 
-const Card = ({ projectTitle, client, year, description, tags, link, images, src, url, i }) => {
+const Card = ({
+  projectTitle,
+  category,
+  client,
+  year,
+  description,
+  tags,
+  link,
+  images,
+  i,
+}) => {
   // TODO: make responive !!
   // TODO: only show project and year columns on smaller screens
   return (
     <div className={styles.cardContainer}>
       {/* TODO: change the top to show a little more */}
-      <div className={styles.card} style={{ top: `calc(-5vh + ${i * 25}px)` }}>
+      <div
+        className={styles.card}
+        style={{
+          top: `calc(-5vh + ${i * 3}em)`,
+          // top: `calc(20vh + 5.75em);margin-bottom:11.5em`,
+        }}
+      >
         {/* A dynamic top position is set depending on the index of each cards, creating a simple stacking effect.
          And that's how the color of each card is set. */}
-
         <div className="borderr w-full flex justify-between">
-          <p>00{i + 1}</p>
+          <p className=" font-bold">00{i + 1}</p>
           <p>{projectTitle}</p>
+          <p>{category}</p>
+          <p>{year}</p>
         </div>
         <div className={styles.body}>
           <div className={styles.description}>
             <p>{description}</p>
             <span>
-              <a href={url} target="_blank">
-                See more
+              <a href={link} target="_blank">
+                Visit Site <ArrowTopRightIcon />
               </a>
-              <svg
-                width="22"
-                height="12"
-                viewBox="0 0 22 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989592 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75L21 6.75V5.25L0 5.25L0 6.75Z"
-                  fill="black"
-                />
-              </svg>
             </span>
           </div>
 
           <div className={styles.imageContainer}>
-            <div className={styles.inner}>
-              <Image fill src={`/${src}`} alt="image" />
-            </div>
+            {images.map((src, i) => {
+              return (
+                <div key={`img_${i}`} className={styles.inner}>
+                  <Image fill src={`/${src}`} alt="image" />
+                </div>
+              );
+            })}
+          </div>
+          <div className={styles.imageContainer}>
+            {images.map((src, i) => {
+              return (
+                <div key={`img_${i}`} className={styles.inner}>
+                  <Image fill src={`/${src}`} alt="image" />
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
