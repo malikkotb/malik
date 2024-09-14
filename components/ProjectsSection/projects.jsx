b33d571da39835ae70636134da710e73c39c5a8b
malikkotb <malikkotb@icloud.com>
2024-09-14 11:29:26 +0200

    project cards

--- components/ProjectsSection/projects.jsx
+++ components/ProjectsSection/projects.jsx
@@ -1,15 +1,12 @@
 "use client";
 import projects from "../../app/projects";
-import Card from "./Card";
+import Card from "./Card/Card";
 export default function Projects() {
   return (
-    <div
-      style={{
-        border: "1px solid blue",
-      }}
-      className="h-screen mt-[50vh] mb-[50vh]"
-    >{projects.map((project, i) => {
-        return <Card key={`p_${i}`} {...project} i={i} />
-    })}</div>
+    <div className="mt-[50vh] mb-[50vh]">
+      {projects.map((project, i) => {
+        return <Card key={`p_${i}`} {...project} i={i} />;
+      })}
+    </div>
   );
 }
