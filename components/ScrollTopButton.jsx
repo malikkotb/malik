0d8b1dd0152a7f906f6c754b67f883b303fec502
malikkotb <malikkotb@icloud.com>
2024-09-21 15:54:16 +0200

    magnetic button

--- components/ScrollTopButton.jsx
+++ components/ScrollTopButton.jsx
@@ -1,17 +1,18 @@
+import MagneticButton from "./MagneticButton";
 const ScrollToTopButton = () => {
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  //   TODO: make the button magnetic and come up from bottom like the zoop links
-
   return (
-    <button
-      onClick={handleScrollToTop}
-      className="p-3 px-4 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600"
-    >
-      ↑
-    </button>
+    <MagneticButton>
+      <button
+        onClick={handleScrollToTop}
+        className="p-3 px-4 bg-white rounded-full shadow-lg text-black"
+      >
+        ↑
+      </button>
+    </MagneticButton>
   );
 };
 
