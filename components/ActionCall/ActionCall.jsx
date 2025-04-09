a7d8f0d657d717f884d709f95b3c6b9d2c3609c6
Malik 3 <malikkotb@icloud.com>
2025-04-09 11:27:38 +0200

    action call

--- components/ActionCall/ActionCall.jsx
+++ components/ActionCall/ActionCall.jsx
@@ -1,19 +1,20 @@
 "use client";
-
+import TextFadeGradient from "../TextFadeGradient/TextFadeGradient";
 export default function ActionCall() {
   // TODO: opacity animation on scroll for this text
   // like https://www.spylt.com/ section
   return (
     <div className='h-[100vh] flex gap-12 flex-col px-[5vw] justify-center items-center'>
       <h3 className='h3'>
-        {/* <h2 className='h3 text-white w-fit font-bold text-center font-neuemontreal-bold leading-[100%] tracking-[-3%] text-[8vw] uppercase'> */}
-        Everyone has a <span className='text-[#909090]'>story</span>,
-        let me help you tell{" "}
-        <span className='text-[#909090]'>yours</span>.
+        <TextFadeGradient
+          paragraph={
+            "Everyone has a story, let me help you tell yours."
+          }
+        />
       </h3>
       <a
         href='mailto:hello@malikkotb.com'
-        className='text-black px-6 py-5 bg-white font-neuemontreal-bold leading-[100%] tracking-[-3%] text-[2vw]'
+        className='text-black px-6 py-5 bg-white font-neuemontreal-bold leading-[100%] tracking-[-0.5px] text-[2vw]'
       >
         BOOK A CALL
       </a>
--- components/TextFadeGradient/TextFadeGradient.jsx
+++ components/TextFadeGradient/TextFadeGradient.jsx
@@ -10,18 +10,20 @@ export default function TextFadeGradient({ paragraph }) {
     marker: true,
   });
 
-  // INSPIRED BY: https://www.rayraylab.com/
-
   const words = paragraph.split(" ");
   return (
-    <div className="h-screen sticky flex justify-center section-padding items-center">
-      <div className="w-[80%] borderr text-5xl text-center">
+    <div className='h-fit flex justify-center section-padding items-center'>
+      <div className='w-[80%] borderr h3 text-white text-center'>
         <p ref={container} className={styles.paragraph}>
           {words.map((word, i) => {
             const start = i / words.length;
             const end = start + 1 / words.length;
             return (
-              <Word key={i} progress={scrollYProgress} range={[start, end]}>
+              <Word
+                key={i}
+                progress={scrollYProgress}
+                range={[start, end]}
+              >
                 {word}
               </Word>
             );
@@ -33,29 +35,47 @@ export default function TextFadeGradient({ paragraph }) {
 }
 
 const Word = ({ children, progress, range }) => {
-  const amount = range[1] - range[0];
-  const step = amount / children.length;
+  const opacity = useTransform(progress, range, [0, 1]);
+
   return (
     <span className={styles.word}>
-      {children.split("").map((char, i) => {
-        const start = range[0] + i * step;
-        const end = range[0] + (i + 1) * step;
-        return (
-          <Char key={`c_${i}`} progress={progress} range={[start, end]}>
-            {char}
-          </Char>
-        );
-      })}
+      <span className={styles.shadow}>{children}</span>
+      <motion.span style={{ opacity }}>{children}</motion.span>
+      &nbsp;
     </span>
   );
 };
 
-const Char = ({ children, progress, range }) => {
-  const opacity = useTransform(progress, range, [0, 1]);
-  return (
-    <span>
-      <span className={styles.shadow}>{children}</span>
-      <motion.span style={{ opacity: opacity }}>{children}</motion.span>
-    </span>
-  );
-};
+// const Word = ({ children, progress, range }) => {
+//   const amount = range[1] - range[0];
+//   const step = amount / children.length;
+//   return (
+//     <span className={styles.word}>
+//       {children.split("").map((char, i) => {
+//         const start = range[0] + i * step;
+//         const end = range[0] + (i + 1) * step;
+//         return (
+//           <Char
+//             key={`c_${i}`}
+//             progress={progress}
+//             range={[start, end]}
+//           >
+//             {char}
+//           </Char>
+//         );
+//       })}
+//     </span>
+//   );
+// };
+
+// const Char = ({ children, progress, range }) => {
+//   const opacity = useTransform(progress, range, [0, 1]);
+//   return (
+//     <span>
+//       <span className={styles.shadow}>{children}</span>
+//       <motion.span style={{ opacity: opacity }}>
+//         {children}
+//       </motion.span>
+//     </span>
+//   );
+// };
