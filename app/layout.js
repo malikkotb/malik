95447ae41ae1d8a6233d0acb7b0e6d4f4dfa812a
Malik <malikkotb@icloud.com>
2026-02-13 22:56:05 +0100

    analyitcs

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,23 @@
-import "./globals.css";
-import Header from "@/components/Header";
-import LoadingScreen from "@/components/LoadingScreen";
-import TransitionLayout from "@/components/TransitionLayout";
+import './globals.css';
+import Header from '@/components/Header';
+import LoadingScreen from '@/components/LoadingScreen';
+import TransitionLayout from '@/components/TransitionLayout';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata = {
-  title: "Index | Malik Kotb",
-  description: "Designed & Developed by Malik Kotb",
+  title: 'Index | Malik Kotb',
+  description: 'Designed & Developed by Malik Kotb',
   icons: {
-    icon: "/fav.png",
-    shortcut: "/fav.png",
-    apple: "/fav.png",
+    icon: '/fav.png',
+    shortcut: '/fav.png',
+    apple: '/fav.png',
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <Analytics />
       <body className={`antialiased`}>
         <TransitionLayout>
           <Header />
